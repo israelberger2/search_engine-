3c1c363d8b0d5ce3c0e54fd26f16ebb7611cced7
israelberger2 <israelberger20@gmail.com>
2023-08-01 22:34:03 +0300

    n the middle of rewriting the searcher

--- includes/searcher.hpp
+++ includes/searcher.hpp
@@ -1,20 +1,54 @@
-#ifndef SEARCHER_HPP
-#define SEARCHER_HPP
+// #ifndef SEARCHER_HPP
+// #define SEARCHER_HPP
+
+// #include <vector>
+// #include <string>
+// #include  <utility>
+
+
+// namespace se{
+
+// class Searcher
+// {
+// public:
+//     virtual ~Searcher() = default;
+    
+//     virtual std::vector<std::pair<std::string, size_t>> get_links(const std::vector<std::string>& querie)const = 0;
+// };
+
+// } // namespace se
+// #endif
+
+#ifndef SEARCHER_HPP 
+#define SEARCHER_HPP 
 
 #include <vector>
 #include <string>
-#include  <utility>
 
+#include "word_data.hpp"
+#include "links_data.hpp"
 
+ 
 namespace se{
-
+ 
 class Searcher
 {
 public:
-    virtual ~Searcher() = default;
-    
-    virtual std::vector<std::pair<std::string, size_t>> get_links(const std::vector<std::string>& querie)const = 0;
-};
+    Searcher(db::WordData& wordData, db::LinksData& linkData);
+    ~Searcher() = default;
+    Searcher(const Searcher&) = default;
+    Searcher& operator= (const Searcher&) = default;
 
-} // namespace se
+    std::vector<int> search(const std::vector<std::string>& positiveWords, const std::vector<std::string>& negativeWords)const;
+
+private:
+    bool validLink(const std::vector<int>& wordsID, int linksID, bool existingWordsSearch);
+
+private:
+    db::WordData& m_wordData;
+    db::LinksData& m_linkData;
+};
+ 
+} //se
 #endif
+
--- main.cpp
+++ main.cpp
@@ -33,7 +33,7 @@
 using namespace se;
  
 
-bool fff(const std::vector<int>& wordsID, int linksID)
+bool exist(const std::vector<int>& wordsID, int linksID, bool existingWordsSearch)
 {
   db::Connector connector{};
   
@@ -42,7 +42,9 @@ bool fff(const std::vector<int>& wordsID, int linksID)
 
   std::string query  = "SELECT COUNT(WordLink.LinkID) FROM WordLink WHERE WordLink.LinkID = ?  and (WordLink.WordID = ? ";
   std::string appendString;
-  for(size_t i = 1; i < wordsID.size(); ++i){
+  for(size_t i = 1; i < wordsID.size(); ++i){ 
+    std::cout << "iteration" << '\n';
+       
     std::string appendString = " OR WordLink.WordID = ?";
     query.append(appendString);
   }
@@ -52,53 +54,110 @@ bool fff(const std::vector<int>& wordsID, int linksID)
 
   stmt->setInt(1, linksID);
   for(size_t i = 0; i < wordsID.size(); ++i){
+    std::cout << "set iteration" << '\n';
+    
     stmt->setInt(i+2, wordsID[i]);
   }
-
+  std::cout << "befor result(stmt->executeQuery());" << '\n';
+  
   std::unique_ptr<sql::ResultSet> result(stmt->executeQuery());
 
   size_t count = 0;
+  std::cout << "befor result" << '\n';
  
   while(result->next()){
+    std::cout << "result->next" << '\n';
+    
     count = result->getInt(1);
   }
-
-  return count == wordsID.size() ? true : false;
-}
-
-std::vector<int> check(const std::vector<int>& wordsID, const std::vector<int>& linksID)
-{
-  if(wordsID.empty() || linksID.empty()){
-    return linksID;
+  
+  if(existingWordsSearch){
+    return count == wordsID.size() ? true : false;
   }
 
-  std::vector<int> validLinks;
+  return !count ? true : false;
+}
 
-  for(auto& link : linksID){
-    if(fff(wordsID, link)){
-      validLinks.push_back(link);
-      std::cout << "valid" << '\n';
-    } else{
-      std::cout << "invalide" << '\n';
+// std::vector<int> check(const std::vector<int>& positiveWords, const std::vector<int>& negativeWords, const std::vector<int>& linksID)
+// {
+//   if(positiveWords.empty() || linksID.empty()){
+//     return linksID;
+//   }
+
+//   std::vector<int> linksContainWords;
+
+//   for(auto link : linksID){
+//     if(exist(positiveWords, link, true)){
+//       linksContainWords.push_back(link);
+//     }  
+//   }
+
+//   std::vector<int> result;
+
+//   for(auto link : linksContainWords){
+//     if(exist(negativeWords, link, false)){
+//       result.push_back(link);
+//     }
+//   }
+
+//   return result;
+// }
+ 
+ std::vector<int> check(const std::vector<std::string>& positiveWords, const std::vector<std::string>& negativeWords)
+{
+  db::MysqlWordData wordData{};
+  db::MysqlWordLinks wordlinks{};
+  std::vector<int> linksID = wordlinks.getLinksForWord(positiveWords[0]);
+  
+  std::vector<int> linksContainWords;
+  
+  if(positiveWords.size() > 1){
+    std::vector<int> wordsID = wordData.getWordsID(std::vector<std::string>{positiveWords.begin()+1, positiveWords.end()});
+    
+    if(wordsID.size() < positiveWords.size()-1){
+      return std::vector<int>{};
+    }
+
+    for(auto link : linksID){
+      if(exist(wordsID, link, true)){
+        linksContainWords.push_back(link);
+      }  
+    }
+  } else{
+    linksContainWords = linksID;
+  }
+
+  std::vector<int> result;
+  std::vector<int> negativeWordsID = wordData.getWordsID(negativeWords);
+  if(negativeWordsID.empty()){
+    return linksContainWords;
+  } 
+ 
+  for(auto link : linksContainWords){
+    if(exist(negativeWordsID, link, false)){
+      result.push_back(link);
     }
   }
 
-  return validLinks;
+  return result;
 }
+
 
 int main(int argc, char* argv[]) 
 {
-  db::MysqlWordData wordData{};
-  std::vector<int> wordsID = wordData.getWordsID(std::vector<std::string>{"Potter", "some", "than" });
-
-  db::MysqlWordLinks wl{};
-  std::vector<int> linksID = wl.getLinksForWord("Ball");
-
-  for(auto link : linksID){
-    std::cout << "links: " << link << '\n';
+  std::vector<std::string> positive = {"Harry", "Potter"};
+  std::vector<std::string> negative = {"Ballddddddd", "Ball", "DSss"};
+  std::cout << "size of negative: " << negative.size() << '\n';
+  for(auto n : negative){
+    std::cout << "ne:  " << n << '\n';
     
   }
-  std::vector<int> result = check(wordsID, linksID);
+  
+    
+   
+  std::vector<int> result = check(positive, negative);
+  std::cout << ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,," << '\n';
+  
   for(auto res : result){
     std::cout << "id: " << res << '\n';
     
