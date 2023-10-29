3eb1a350027674d57aa5740e5e5b7ec16a1c9f51
israelberger2 <israelberger20@gmail.com>
2023-10-29 15:07:08 +0200

    fix socket

--- src/socket.cpp
+++ src/socket.cpp
@@ -9,7 +9,7 @@ Socket::Socket()
     init();
     create_socket();
     int listenStatus = listen(m_fileDiscriptor, 10);
-    if(! listenStatus){
+    if(listenStatus < 0){        
         throw SocketError("ERROR: listen function is fail");
     }
 }
@@ -25,12 +25,12 @@ void Socket::create_socket()
 {
     m_fileDiscriptor = socket(AF_INET, SOCK_STREAM, 0);
     
-    if(m_fileDiscriptor < 0){
+    if(m_fileDiscriptor < 0){        
         throw SocketError("ERROR: unsuccessful to create socket and socket was fail");
     }
   
     int bindStatus = bind(m_fileDiscriptor, (struct sockaddr*)&m_server_addr, sizeof(m_server_addr));
-    if (! bindStatus){
+    if (bindStatus < 0){
         int yes = 1;
         if (setsockopt(m_fileDiscriptor, SOL_SOCKET, SO_REUSEADDR, (void*)&yes, sizeof(yes)) < 0){
             throw SocketError("unsuccessful to create socket and setsockopt function was fail");
@@ -44,7 +44,7 @@ std::shared_ptr<Communicator> Socket::creat_communicator()
     socklen_t client_addr_size = sizeof(client_addr);
 
     int client_socket = accept(m_fileDiscriptor, (struct sockaddr*)&client_addr, &client_addr_size);
-    if(! client_socket){
+    if(client_socket < 0){
         throw SocketError("ERROR: accept function is fail");
     }
 
