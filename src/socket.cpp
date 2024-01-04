871f411267a016ce84b42b7d17b1853d2c99bdb5
israelberger2 <israelberger20@gmail.com>
2024-01-04 11:36:22 +0200

    fix socket

--- src/socket.cpp
+++ src/socket.cpp
@@ -1,50 +1,56 @@
-#include  "json.hpp" 
-
-#include "net_client.hpp"
+#include "socket.hpp"
 #include "se_exceptions.hpp"
+#include "configuration.hpp"
 
 
 namespace se{
 
-using json = nlohmann::json;
-
-NetClient::NetClient()
-: m_socket()
-, m_communicator(nullptr)
-{}
-
-std::vector<std::string> NetClient::load_query()
-{           
-    try{
-        m_communicator =  m_socket.creat_communicator();                
-    } catch (const SocketError& error){        
-        throw SocketError(error.what());
-    }
-
-    std::vector<std::string> res;
-    
-    try{
-        std::string query = m_communicator->receive_data();
-         
-        json js = json::parse(query);
-        res = js.get<std::vector<std::string>>();        
-        return res;   
-    } catch (const json::parse_error& error) {
-        throw DataError(std::string("ERROR: json::runtime_error: " + std::string(error.what())));  
+Socket::Socket()
+{
+    init();
+    create_socket();
+    int listenStatus = listen(m_fileDiscriptor, 10);
+    if(listenStatus < 0){        
+        throw SocketError("ERROR: the listen function failed");
     }
 }
 
-void NetClient::send_data(const std::vector<std::pair<std::string, int>>& links)const
-{    
-    json j = links;
-    std::string data = j.dump();
-    const char* buffer = data.c_str();
+void Socket::init()
+{
+    m_server_addr.sin_family = AF_INET;
+    m_server_addr.sin_port = htons(Config::getPort());
+    m_server_addr.sin_addr.s_addr = INADDR_ANY;
+}
 
-    try{
-        m_communicator->send_data(buffer, data.length());
-    } catch (const NetworkError& error){
-        throw NetworkError(error.what());
-    }   
+void Socket::create_socket()
+{
+    m_fileDiscriptor = socket(AF_INET, SOCK_STREAM, 0);
+    
+    if(m_fileDiscriptor < 0){        
+        throw SocketError("ERROR: unsuccessful to create socket and the socket failed");
+    }
+  
+    int bindStatus = bind(m_fileDiscriptor, (struct sockaddr*)&m_server_addr, sizeof(m_server_addr));
+    if (bindStatus < 0){
+        int yes = 1;
+        if (setsockopt(m_fileDiscriptor, SOL_SOCKET, SO_REUSEADDR, (void*)&yes, sizeof(yes)) < 0){
+            throw SocketError("unsuccessful to create socket and the setsockopt function failed");
+        }
+    }
+}
+
+std::shared_ptr<Communicator> Socket::creat_communicator()const
+{
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_size = sizeof(client_addr);
+
+    int client_socket = accept(m_fileDiscriptor, (struct sockaddr*)&client_addr, &client_addr_size);
+    if(client_socket < 0){
+        throw SocketError("ERROR: the accept function is failed");
+    }
+
+    std::shared_ptr<Communicator> communicator = std::make_shared<Communicator>(client_socket);
+    return communicator;
 }
 
 } // namespace se
