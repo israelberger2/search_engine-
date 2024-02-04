9371e54231c90f2f77003eff38c45f784930c3cc
israelberger2 <israelberger20@gmail.com>
2024-02-04 11:25:43 +0200

    main fix

--- test.cpp
+++ test.cpp
@@ -1,18 +1,57 @@
 #include <iostream>
-#include <bitset>
-
-int main() {
-    int x = 42;
-    char* c = (char*)&x;
-    printf("%d \n", c[0]);
-    printf("%d \n", c[1]);
-    printf("%d \n", c[2]);
-    printf("%d \n", c[3]);
+#include <vector>
 
 
-    return 0;
+bool polindrom(char** metrix, int row, int column)
+{
+    int firstR = 0;
+    int firstC = 0;
+    int endR = row -1;
+    int endC = column -1;
+
+    while (endR > firstR || (firstR == endR && firstC < endC) ){        
+       if(metrix[firstR][firstC] != metrix[endR][endC]){        
+        return false;
+       }
+       if(firstC == column-1){
+        firstR = 0;
+        ++firstR;
+       }else{
+        ++firstC;
+       }
+
+        if(endC == 0){
+            endC = column -1;
+            --endR;
+        }else{
+            --endC;
+        }
+    }
+
+    return true;
 }
 
+int main()
+{
+    char** mtx = new char*[4];
+
+    for(int i = 0; i < 4; ++i){
+        mtx[i] = new char[4];
+    }
 
 
- 
+    char c ='a';
+    for(int i = 0; i < 4; ++i){
+        std::cout << i << '\n';
+        
+        **(mtx +i) = c++ ;
+    }
+
+    for(int i = 0; i < 4; ++i){
+       std::cout << i << " char: " << **(mtx +i) << '\n';
+    }
+    //  = {{'a', 't', 'e', 'r'}, {'r', 'e', 't','a'}};
+    // std::cout << polindrom(mtx, 2, 4) << '\n';
+     
+    return 0;
+}
